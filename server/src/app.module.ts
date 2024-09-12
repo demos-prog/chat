c2730954ee3efc070c01bf5c56828722a3af8bf9
demos-prog <73884833+demos-prog@users.noreply.github.com>
2024-09-12 09:08:01 +0300

    del

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -15,7 +15,13 @@ import { ChatModule } from './chat/chat.module';
     }),
     SequelizeModule.forRoot({
       dialect: 'postgres',
-      uri: process.env.DATABASE_URL,
+      autoLoadModels: true,
+      synchronize: true,
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
       models: [Message]
     }),
     ChatModule,
