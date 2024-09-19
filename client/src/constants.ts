d0f294089e5744b1dd54d0cf3fd77196fae5a3d6
demos-prog <73884833+demos-prog@users.noreply.github.com>
2024-09-19 14:14:07 +0300

    cors

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,4 +1,5 @@
 export const SOCKET_URI = 'http://localhost:3000';
+// export const SOCKET_URI = 'https://chatserver-g18j03k7s-demosprogs-projects.vercel.app/';
 
 export type Message = {
   author: string;
