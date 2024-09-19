33a1dfd12bd600fa9cbfe8b5adea6c0bc901dad1
demos-prog <73884833+demos-prog@users.noreply.github.com>
2024-09-19 13:53:15 +0300

    relative paths

--- server/src/chat/chat.module.ts
+++ server/src/chat/chat.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ChatGateway } from './chat.gateway';
 import { Message } from '../messages/entities/message.entity';
-import { MessagesService } from 'src/messages/messages.service';
+import { MessagesService } from '../messages/messages.service';
 
 @Module({
   imports: [
